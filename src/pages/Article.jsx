6d0041282f605e1ce2ecff4d4ac3382a8eedf95a
Binderiya <binderiyatsogthers@gmail.com>
2023-01-17 18:57:22 +0800

    first commit

---
+++ src/pages/Article.jsx
@@ -0,0 +1,11 @@
+import SlimNav from "../components/Article/SlimNav";
+import Main from "../components/Article/Main";
+
+export default function Article() {
+  return (
+    <>
+      <SlimNav />
+      <Main />
+    </>
+  );
+}
